4e89fe2b15f26e8e36e9354741714e7c890ee134
Marcio Maia <marcio.luis.maia@gmail.com>
2026-05-26 16:38:58 -0400

    fixing warning

--- OCP1Z0831Learning/srcs/modulo1/module-info.java
+++ OCP1Z0831Learning/srcs/modulo1/module-info.java
@@ -1,3 +1,12 @@
 
-module mnModulo1 {
+module mnModulo1m {
 }
+
+/*
+
+srcs\modulo1\module-info.java:2: warning: [module] module name component mnModulo1 should avoid terminal digits
+module mnModulo1 {
+       ^
+1 warning
+
+*/
