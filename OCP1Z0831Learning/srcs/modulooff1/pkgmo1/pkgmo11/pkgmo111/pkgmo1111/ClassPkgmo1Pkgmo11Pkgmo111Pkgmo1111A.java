5401ad6516741e356d0fda3b67e4517536d4a425
Marcio Maia <marcio.luis.maia@gmail.com>
2026-05-27 14:29:05 -0400

    turning class ClassPkgmo1Pkgmo11Pkgmo111Pkgmo1111A executable

--- OCP1Z0831Learning/srcs/modulooff1/pkgmo1/pkgmo11/pkgmo111/pkgmo1111/ClassPkgmo1Pkgmo11Pkgmo111Pkgmo1111A.java
+++ OCP1Z0831Learning/srcs/modulooff1/pkgmo1/pkgmo11/pkgmo111/pkgmo1111/ClassPkgmo1Pkgmo11Pkgmo111Pkgmo1111A.java
@@ -4,4 +4,12 @@ public class ClassPkgmo1Pkgmo11Pkgmo111Pkgmo1111A {
 	public static String s = "moduloMO1-pkgmo1-pkgmo11-pkgmo111-pkgmo1111-ClassPkgmo1Pkgmo11Pkgmo111Pkgmo1111A";
 	public static String s2 = pkg3.pkg31.pkg311.Classpkg3pkg31pkg311A.s;
 	public static String s3 = pkg3.pkg31.pkg311.pkg3111.ClassPkg3pkg31pkg311pkg3111A.s;
+	public static String s4 = pkgmo3.pkgmo31.pkgmo311.Classpkgmo3pkgmo31pkgmo311A.s;
+	public static void main(String ... args){
+		System.out.println("[pkgmo1.pkgmo11.pkgmo111.pkgmo1111][ClassPkgmo1Pkgmo11Pkgmo111Pkgmo1111A][public static void main(String ... args)][HW...]");
+		System.out.println("[s="+(s)+"]");
+		System.out.println("[s2="+(s2)+"]");
+		System.out.println("[s3="+(s3)+"]");
+		System.out.println("[s4="+(s4)+"]");
+	}
 }
