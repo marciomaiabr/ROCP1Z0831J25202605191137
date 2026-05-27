5a794a80a7016e9a56e8affd9d2aa3be4ae99992
Marcio Maia <marcio.luis.maia@gmail.com>
2026-05-27 12:34:05 -0400

    adding modulooff3

---
+++ OCP1Z0831Learning/srcs/modulooff3/pkgmo3/pkgmo31/pkgmo311/pkgmo3111/ClassPkgmo3pkgmo31pkgmo311pkgmo3111A.java
@@ -0,0 +1,5 @@
+package pkgmo3.pkgmo31.pkgmo311.pkgmo3111;
+
+public class ClassPkgmo3pkgmo31pkgmo311pkgmo3111A {
+	public static String s = "modulo1-pkgmo3-pkgmo31-pkgmo311-pkgmo3111-Classpkgmo3pkgmo31pkgmo311pkgmo3111A";
+}
