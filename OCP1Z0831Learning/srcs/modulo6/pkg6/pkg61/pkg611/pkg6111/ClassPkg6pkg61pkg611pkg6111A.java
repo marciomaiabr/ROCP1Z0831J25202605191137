35ff5d019e00459a2aa6a3022a40e3ad8565d2bd
Marcio Maia <marcio.luis.maia@gmail.com>
2026-05-27 13:56:50 -0400

    alterando ClassPkg6pkg61pkg611pkg6111A consumindo modulooff2

--- OCP1Z0831Learning/srcs/modulo6/pkg6/pkg61/pkg611/pkg6111/ClassPkg6pkg61pkg611pkg6111A.java
+++ OCP1Z0831Learning/srcs/modulo6/pkg6/pkg61/pkg611/pkg6111/ClassPkg6pkg61pkg611pkg6111A.java
@@ -3,9 +3,11 @@ package pkg6.pkg61.pkg611.pkg6111;
 public class ClassPkg6pkg61pkg611pkg6111A {
 	public static String s = "modulo6-pkg6-pkg61-pkg611-pkg6111-Classpkg6pkg61pkg611pkg6111A";
 	public static String ss = pkg1.pkg11.pkg111.pkg1111.ClassPkg1Pkg11Pkg111Pkg1111A.s;
+	public static String sss = pkgmo2.pkgmo21.pkgmo211.pkgmo2111.ClassPkgmo2pkgmo21pkgmo211pkgmo2111A.s;
 	public static void main(String ... args){
 		System.out.println("[pkg6.pkg61.pkg611.pkg6111][ClassPkg6pkg61pkg611pkg6111A][public static void main(String ... args)][HW...]");
 		System.out.println("[s="+(s)+"]");
 		System.out.println("[ss="+(ss)+"]");
+		System.out.println("[sss="+(sss)+"]");
 	}
 }
