4160ac4f7a9c7fb4a3976f5413ba67c53737b36b
Marcio Maia <marcio.luis.maia@gmail.com>
2026-05-28 11:19:31 -0400

    testing first impl

---
+++ OCP1Z0831Learning/srcs/moduloImpl1Prod/br/com/pkgModuloImpl1Prod/ClassModuloImpl1Prod.java
@@ -0,0 +1,6 @@
+package br.com.pkgModuloImpl1Prod;
+
+import br.com.pkgModuloInterfaces.InterfaceX;
+
+public class ClassModuloImpl1Prod implements InterfaceX {
+}
---
+++ OCP1Z0831Learning/srcs/moduloImpl1Prod/module-info.java
@@ -0,0 +1,5 @@
+
+module mnModuloImpl1Prod {
+	requires mnModuloInterfaces;
+	provides br.com.pkgModuloInterfaces.InterfaceX with br.com.pkgModuloImpl1Prod.ClassModuloImpl1Prod;
+}
